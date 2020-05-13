9f00f8b3a5bafc3160b236ca07660e8b07bc21be
toilet <mincokoornstra2@gmail.com>
2020-05-13 17:20:09 +0200

    translated dienblad to Tray

---
+++ KantineSimulatieBasis/src/main/java/Tray.java
@@ -0,0 +1,46 @@
+package main.java;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Stack;
+
+
+public class Tray {
+    private ArrayList<Artikel> articles;
+
+    /**
+     * Constructor
+     */
+    public Tray() {
+        articles = new ArrayList<>();
+    }
+
+    /**
+     * Method to add Artikel to Dienblad
+     *
+     * @param article
+     */
+    public void addTo(Artikel article) {
+        // method body omitted
+    }
+
+    /**
+     * Method to get amount of articles on tray
+     *
+     * @return Amount of articles
+     */
+    public int getArticleAmount() {
+        // method body omitted
+    }
+
+    /**
+     * Method to get the total price of the articles on the tray
+     *
+     * @return Total price
+     */
+    public double getTotalPrice() {
+        // method body omitted
+    }
+}
+
