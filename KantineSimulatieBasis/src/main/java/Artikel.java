d867f9ca274056e8f9d692ba057cd1b6c5951765
toilet <mincokoornstra2@gmail.com>
2020-05-13 17:07:11 +0200

    class artikel added

---
+++ KantineSimulatieBasis/src/main/java/Artikel.java
@@ -0,0 +1,58 @@
+package main.java;
+
+
+import java.util.ArrayList;
+
+public class Artikel {
+    float price;
+    String name;
+
+    public Artikel(){
+
+
+    }
+
+    public Artikel(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+    //Getters and setters for Price and Name
+
+    /**
+     * Getter for Price
+     *
+     * @return
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * Getter for Name
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter for price
+     *
+     * @param price
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * Setter for Name
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
